84273d2337ed7d66f71128573ac123937f8307f9
Vanya diamondra <vanyadiamondra@gmail.com>
2023-01-20 16:41:00 +0100

    delete

--- src/main/java/vae/vae/repository/DemandeRechargementRepository.java
+++
@@ -1,8 +0,0 @@
-package vae.vae.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import vae.vae.model.DemandeRechargement;
-
-public interface DemandeRechargementRepository extends MongoRepository<DemandeRechargement,Integer>, CustomMiseEnchereRepository {
-
-}
--- src/main/java/vae/vae/repository/MiseEnchereRepository.java
+++
@@ -1,8 +0,0 @@
-package vae.vae.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import vae.vae.model.MiseEnchere;
-
-public interface MiseEnchereRepository extends MongoRepository<MiseEnchere,Integer>, CustomMiseEnchereRepository {
-
-}
