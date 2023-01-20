c982f89e9a46ee29fcbaa89190e31ba942bba640
Vanya diamondra <vanyadiamondra@gmail.com>
2023-01-20 16:46:35 +0100

    push delete

--- src/main/java/vae/vae/repository/UtilisateursRepository.java
+++
@@ -1,9 +0,0 @@
-package vae.vae.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vae.vae.model.Utilisateurs;
-
-public class UtilisateursRepository {
-//public interface UtilisateursRepository extends JpaRepository<Utilisateurs,Integer> {
-}
-
